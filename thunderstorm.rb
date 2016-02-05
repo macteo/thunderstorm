5c323f1f3af55f7aeef74099b3a43b67d016a313
Matteo Gavagnin <m@macteo.it>
2016-02-05 21:49:11 +0100

    Embryonal readme.

--- thunderstorm.rb
+++ thunderstorm.rb
@@ -2,14 +2,9 @@ class Thunderstorm < Formula
   desc "Open source CLI for buford to send push notifications to Apple devices through HTTP/2"
   homepage "https://github.com/macteo/thunderstorm"
   url "https://github.com/macteo/thunderstorm.git",
-    :tag => "v0.1.1" #, :revision => "8a8336ae08b3ac5bb4ddce4a5ebbf16049961ad2"
+    :tag => "v0.1.3"
 
   head "https://github.com/macteo/thunderstorm.git"
-
-  # bottle do
-  #   cellar :any_skip_relocation
-  #   sha256 "09d708d36d4a1267de3d99f8318412ded60e7549d8915e079e2a2e522f29330f" => :el_capitan
-  # end
 
   depends_on "go" => :build
 
